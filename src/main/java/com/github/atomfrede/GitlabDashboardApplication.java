b92bc35de2e70a950715aabecf9fda13b46e6655
Frederik Hahne <frederik.hahne@gmail.com>
2016-04-01 19:30:31 +0200

    initial commit

---
+++ src/main/java/com/github/atomfrede/GitlabDashboardApplication.java
@@ -0,0 +1,12 @@
+package com.github.atomfrede;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitlabDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitlabDashboardApplication.class, args);
+	}
+}
